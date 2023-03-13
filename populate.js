38f1e1bb51e31b08cf09bf5e3d7a3b03e3d6d92f
MaxenceAll <maxence.allart@gmail.com>
2023-03-13 15:09:27 +0100

    first

---
+++ populate.js
@@ -0,0 +1,23 @@
+//petit script pour ajouter les products (products.json) à la db
+require('dotenv').config()
+
+const connectDB = require('./db/connect')
+const Product = require('./models/product')
+
+const jsonProducts = require('./products.json')
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+    await Product.deleteMany()
+    // create is mongoose method
+    await Product.create(jsonProducts)
+    console.log('Success!!!!')
+    process.exit(0)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+start()
